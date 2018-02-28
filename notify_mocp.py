d8eed09d12080fcead363fb7ae984d6f17b53f2a
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-03-01 00:05:16 +0100

    Add files via upload

---
+++ notify_mocp.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import subprocess
+import sys
+import commands
+import os
+import Image
+#Opteniendo informacion artista album y titulo
+artista = commands.getoutput("mocp -Q %artist")
+cancion = commands.getoutput("mocp -Q %song")
+album = commands.getoutput("mocp -Q %album")
+
+#Variables redimensionado
+width = 100
+height = 100
+#Opteniendo path coverart
+fil = commands.getoutput("mocp -Q %file")
+
+#Recortando path
+path = fil.rfind('/')
+if path != -1:
+    path = fil[:path+1] 
+
+#Lista con todos los ficheros del directorio:
+lstDir = os.walk(path)
+ 
+#Crea una lista de los ficheros jpg/png/jpeg que existen en el directorio y los incluye a la lista.
+
+for root, dirs, files in lstDir:
+    for fichero in files:
+        (nombreFichero, extension) = os.path.splitext(fichero)
+        if(extension == ".jpg" or extension == ".png" or extension == ".jpeg"):
+            imagen = '{0}{1}{2}'.format(path, nombreFichero, extension)
+            im1 = Image.open(imagen)
+            img = im1.resize((width, height), Image.ANTIALIAS)
+            img.save(path + nombreFichero + extension)
+            subprocess.call(['notify-send', "--icon=%s" % (imagen), artista + ':' '\n' + cancion + '\n' + album])
