3760df0c8fd9e7fe5821e274649d814839a47e1a
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-10-09 22:08:05 +0200

    Update Mocp.py

--- Mocp.py
+++ Mocp.py
@@ -3,6 +3,7 @@
 
 import requests
 import os
+from re import sub
 
 
 def descargar_img(cantante, disco, directorio):
@@ -12,12 +13,21 @@ def descargar_img(cantante, disco, directorio):
     cantante = cantante.replace('.', '_')
     disco = disco.replace(' ', '_')
     disco = disco.replace('.', '_')
+    disco = disco.replace('?', '_')
+    disco = sub(r"_(Disc[\d*])", "", disco)
+    disco = sub(r"_(CD[\d*])", "", disco)
+    disco = sub(r"_(Cd[\d*])", "", disco)
+    disco = sub(r"_(19[\d**])", "", disco)
+    disco = sub(r"_(20[\d**])", "", disco)
 
     url = "http://images.coveralia.com/audio/%s/" % cantante.lower()[0]
     link = (url + cantante + "-" + disco + "-Frontal.jpg")
+    print(link)
     nombre_local_imagen = (directorio + disco + ".jpg")
+    print(nombre_local_imagen)
     imagen = requests.get(link).content
     with open(nombre_local_imagen, 'wb') as handler:
         handler.write(imagen)
     if os.stat(nombre_local_imagen).st_size <= 345:
         os.remove(nombre_local_imagen)
+
